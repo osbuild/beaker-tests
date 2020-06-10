1959a3efb6afe475fb340e4769e27724d07a5e1d
Major Hayden <major@redhat.com>
2020-06-10 07:35:40 -0500

    Add installation task

---
+++ install/runtest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Set variables.
+source /etc/os-release
+
+# Ensure the repository is working properly.
+sudo dnf repository-packages osbuild-mock list
+
+# Install the Image Builder packages.
+dnf -y install cockpit cockpit-composer composer-cli osbuild osbuild-ostree \
+    osbuild-composer osbuild-composer-rcm osbuild-composer-tests \
+    osbuild-composer-worker python3-osbuild
+
+# Start services.
+systemctl enable --now osbuild-rcm.socket
+systemctl enabme --now osbuild-composer.socket
+systemctl enable --now cockpit.socket
+
+# Verify that the API is running.
+composer-cli status show
+composer-cli sources list
--- mockbuild/README.md
+++
@@ -1,3 +0,0 @@
-# Podman test suite
-Verify that podman can run integration tests with the system. \
-Test Maintainer: [Major Hayden](mailto:major@redhat.com)
